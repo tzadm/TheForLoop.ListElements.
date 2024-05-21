9d4323dcc7ee3ba1f845b44b493b62bee7ab9bc5
tzadm <94993624+tzadm@users.noreply.github.com>
2024-05-21 13:52:03 +0300

    Add files via upload

--- ForLoop.py
+++ ForLoop.py
@@ -2,4 +2,4 @@ my_car = ["BMW", "MB", "LADA", "KIA", "HONDA"]
 cars_count = 0
 for i in my_car:
     cars_count += 10
-    print('Я езжу на автомобиле марки ', i, cars_count, my_car)
+    print('Я езжу на автомобиле марки ', i, cars_count)
